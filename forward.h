ca4195cc083ad27809db094a232523bbed3734dd
jucada11 <juan.paredes@utec.edu.pe>
2025-04-15 23:55:18 -0500

    update forward list

--- forward.h
+++ forward.h
@@ -11,13 +11,9 @@ class ForwardList : public List<T> {
             T data;
             Node* next;
 
-            Node(){ 
-                // TODO
-            }
+            Node() : data(T()), next(nullptr) { }
 
-            Node(T value){
-                // TODO
-            }
+            Node(T value){ data = value; next = nullptr; }
 
             void killSelf(){
                 // TODO      
@@ -29,70 +25,155 @@ class ForwardList : public List<T> {
         int nodes;
 
     public:
-        ForwardList() : List<T>() {}
+        ForwardList() : List<T>(), head(nullptr), nodes(0) { }
 
-        ~ForwardList(){ 
-         	// TODO            
-        }
+        ~ForwardList(){ clear();}
 
         T front(){
-            throw ("sin definir");
+            return head->data;
         }
 
         T back(){
-            throw ("sin definir");
+            Node* temp = head;
+            while (temp->next != nullptr)
+                temp = temp->next;
+            return temp->data;
         }
 
         void push_front(T data){
-            throw ("sin definir");
+            Node* nodo = new Node(data);
+            nodo->data = data;
+            nodo->next = head;
+            head = nodo;
         }
 
         void push_back(T data){
-            throw ("sin definir");
+            Node* nodo = new Node(data);
+            if (is_empty()) {
+                head = nodo;
+            } else {
+                Node* temp = head;
+                while(temp->next != nullptr) {
+                    temp = temp->next;
+                }
+                temp->next = nodo;
+            }
+            nodes++;
         }
 
         T pop_front(){
-            throw ("sin definir");
+            Node* temp = head;
+            T value = head->data;
+            head = head->next;
+            delete temp;
+            nodes--;
+            return value;
         }
 
         T pop_back(){
-            throw ("sin definir");
+            T value;
+            if (head -> next == nullptr) {
+                value = head->data;
+                delete head;
+                head = nullptr;
+            }
+            else {
+                Node* temp = head;
+                while (temp->next->next != nullptr) {
+                    temp = temp->next;
+                }
+                value = temp->next->data;
+                delete temp -> next;
+                temp -> next = nullptr;
+            }
+            nodes--;
+            return value;
         }
 
         T insert(T data, int pos){
-            throw ("sin definir");
+            Node* nodo = new Node(data);
+            Node* temp = head;
+            int i = 0;
+            while (i++ < pos-1) temp = temp->next;
+            nodo-> next = temp->next;
+            temp->next = nodo;
+            nodes++;
+            return data;
         }
 
         bool remove(int pos){
-            throw ("sin definir");
+            Node* temp = head;
+            int i = 0;
+            while (i++ < pos-1) temp = temp->next;
+            Node* nodo = temp->next;
+            temp->next = nodo->next;
+            delete nodo;
+            nodes--;
+            return true;
         }
 
         T& operator[](int pos){
-            throw ("sin definir");
+            Node* temp = head;
+            for (int i = 0; i < pos; i++) {
+                temp = temp->next;
+            }
+            return temp->data;
         }
 
         bool is_empty(){
-            throw ("sin definir");
+            return head == nullptr;
         }
 
         int size(){
-            throw ("sin definir");
+            return nodes;
         }
 
         void clear(){
-            throw ("sin definir");
+            while (head != nullptr) {
+                Node* temp = head;
+                head = head->next;
+                delete temp;
+            }
         }
         
         void sort(){
-            throw ("sin definir");
+            if (!head || !head->next) return;
+            // Usaremos bubble sort
+            bool swapped;
+            do {
+                swapped = false;
+                Node* current = head;
+                while (current->next) {
+                    if (current->data > current->next->data) {
+                        std::swap(current->data, current->next->data);
+                        swapped = true;
+                    }
+                    current = current->next;
+                }
+            } while (swapped);
         }
 
         bool is_sorted(){
-            throw ("sin definir");
+            Node* temp = head;
+            while (temp->next) {
+                if (temp->data > temp->next->data)
+                    return false;
+                temp = temp->next;
+            }
+            return true;
         }
 
         void reverse(){
-            throw ("sin definir");
+            Node* prev = nullptr;
+            Node* current = head;
+            Node* next = nullptr;
+            while (current) {
+                next = current->next;
+                current->next = prev;
+                prev = current;
+                current = next;
+            }
+            head = prev;
         }
 
         std::string name(){
